de7b90591bcb57e30e6170d3953912fbb5bdd890
Victor <1victorjrortiz3@gmail.com>
2026-02-23 10:43:46 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Type a whole number plz:");                              // prints out a message in the application
+            int ranNum = Convert.ToInt32 (Console.ReadLine());                          // 
+            Console.WriteLine("You chose:" + ranNum);                                   // prints out a message and the whole number chosen
+            Console.WriteLine("Do you like video games? (Enter true or false plz):");   // prints out a message in the application
+            bool wGames = Convert.ToBoolean (Console.ReadLine());                       //
+            Console.WriteLine("It is " + wGames + " that i like video games!");         // prints out a message 
+            
         }
     }
 }
