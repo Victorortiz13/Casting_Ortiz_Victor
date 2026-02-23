73c6cd0b859e01822a3c51203c7d9b80e7cf5dd0
Victor <1victorjrortiz3@gmail.com>
2026-02-23 09:18:41 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Casting_Ortiz_Victor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
